5f3943ccbac6fadcc6e04c49b24832475df7d243
dtrager02 <dtrager02@gmail.com>
2022-03-04 01:06:45 -0500

    small bug fixes

--- fill_info.py
+++ fill_info.py
@@ -1,4 +1,5 @@
 import json
+from json.tool import main
 def load():
     with open("info.json","r") as f:
         return json.load(f)
@@ -6,31 +7,38 @@ def load():
 def commit(info):
     with open("info.json","w+") as f:
         return json.dump(info,f)
-    
+ 
+def main_loop():
+    print("Input a common Resume input field and your value in the form 'field:value'. Common fields include 'First' (implying first name attributes),'Country','School'")
+    t = ""
+    while not t == "exit":
+        try: 
+            t = input(">")
+            if(t == "view"):
+                print(current)
+            elif(t == "undo"):
+                current.pop(last)
+            elif(t == "exit"):
+                continue
+            s = t.split(":")
+            current[s[0]] = s[1]
+            last = s[0]
+        except:
+            print("Invalid input")
+            continue
+        print("Added! \n Submit 'exit' to stop.\n Submit 'undo' if you messed up the previous. \n Submit 'view' to see your current field:value pairs.")   
 if __name__ == "__main__":
     current = dict()
     try:
         current =  load()
+        print("Continuing where you left off")
+        main_loop()
+        commit(current)
+        
     except FileNotFoundError:
         print("Existing data not found. Starting new file")
-        t = ""
         print("Input a common Resume input field and your value in the form 'field:value'. Common fields include 'First' (implying first name attributes),'Country','School'")
-        while not t == "exit":
-            try: 
-                t = input(">")
-                if(t == "view"):
-                    print(current)
-                elif(t == "undo"):
-                    current.pop(last)
-                elif(t == "exit"):
-                    continue
-                s = t.split(":")
-                current[s[0]] = s[1]
-                last = s[0]
-            except:
-                print("Invalid input")
-                continue
-            print("Added! \n Submit 'exit' to stop.\n Submit 'undo' if you messed up the previous. \n Submit 'view' to see your current field:value pairs")
+        main_loop()
         commit(current)
             
             
